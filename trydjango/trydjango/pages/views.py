cbf1a66d7d2dbb4aa7dcc4f74786a8c1a7a4fe7c
Felipe Campos <fepas.unb@gmail.com>
2019-03-16 18:26:00 -0300

    inital commit

---
+++ trydjango/trydjango/pages/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [ 123, 1234, 12345]
+    }
+    return render(request, "contact.html", my_context)
